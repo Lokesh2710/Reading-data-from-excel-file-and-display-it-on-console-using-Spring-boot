df2fc52352c34cf55ffe4b5eb3489da31d92a7e4
Lokesh2710 <56870525+Lokesh2710@users.noreply.github.com>
2021-04-29 11:12:21 +0530

    Add files via upload

---
+++ intern/src/main/java/com/jocata/intern/App.java
@@ -0,0 +1,51 @@
+package com.jocata.intern;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.List;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import com.jocata.intern.model.Address;
+import com.jocata.intern.model.Person;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	public static void main( String[] args )
+	{
+		try 
+		{
+			ObjectMapper mapper = new XmlMapper();
+			InputStream inputStream = new FileInputStream(new File("C:\\lekha_data\\data.xml"));
+			TypeReference<List<Person>> typeReference = new TypeReference<List<Person>>() {};
+			List<Person> persons = mapper.readValue(inputStream, typeReference);
+			for(Person p :persons) 
+			{
+				System.out.println("name is "+p.getFirstName()+" city is "
+			+p.getAddress().getCity()+" first car is "+p.getCars()[1]+" age is "+p.getAge());
+		    }
+			
+			/*Person person = new Person();
+	        person.setFirstName("Jack");
+	        person.setLastName("Ryan");
+	        person.setAge(29);
+	        person.setAddress(new Address("12 Street","Kirkland","98052"));
+	        person.setCars(new String[] {"Toyota Camary","Honda Accord"});
+	        
+	        mapper.writeValue(new File("/home/parallels/demo/personsOut.xml"), person);
+	        */
+	        inputStream.close();
+		}
+		catch(Exception e) 
+		{
+			e.printStackTrace();
+		}
+	}
+}
+
